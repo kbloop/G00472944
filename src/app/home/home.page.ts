9293c75ba2f748519cdfaeace8091a3fb08bcf9c
kbloop <kevinppb@gmail.com>
2026-04-30 06:13:57 +0000

    Add student number to header, Add toolbar and searchbar component inside, Add Heart icon & search Icons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonSearchbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {heart, search, searchCircle} from "ionicons/icons";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonSearchbar],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons( { heart, search, searchCircle });
+  }
 }
