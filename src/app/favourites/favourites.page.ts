b6663f4f946e7e620a8ecd79891ef39d214a73d0
kbloop <kevinppb@gmail.com>
2026-05-10 07:06:31 +0000

    Restyle the moviecard component, add some transition effects on hover. Change the Grid layout on the homepage, Add the grid layout to the favourites page.

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { heart, home } from 'ionicons/icons';
 import { MovieCardComponent } from "../movie-card/movie-card.component";
@@ -15,7 +15,7 @@ import { Movie } from '../models/credits';
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [RouterLink, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MovieCardComponent]
+  imports: [RouterLink, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MovieCardComponent, IonRow, IonCol, IonGrid]
 })
 export class FavouritesPage implements OnInit {
   favouritesToDisplay: any;
